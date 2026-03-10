1417374054e94f2410d72fab96dcfcf4a59c0146
PedroHLC1 <89239311+PedroHLC1@users.noreply.github.com>
2026-03-10 18:16:30 -0300

    Initial Commit

---
+++ src/main/java/com/example/sistemasconsultas/SistemasConsultasApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemasconsultas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemasConsultasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemasConsultasApplication.class, args);
+    }
+
+}
